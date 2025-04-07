b814bab3ea976e89c86a0094e399056d2f158c57
Nishant Bhatt <nishantbhatt393@gmail.com>
2025-04-07 10:01:21 +0530

    StaticConstructor&Properties

---
+++ Properties_and_static_const/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties_and_static_const
+{/*static constructor, static class, static method, 
+  * destructor
+  * get, set, properties, static properties, auto implemented property*/
+
+    class A
+    {
+        public static int NUM {  get; set; }
+        static int num;
+        static string start = "Welcome to the program.";
+        static A()
+        {
+            NUM = 334;
+            Console.WriteLine(A.start);
+            Console.WriteLine(NUM);
+        }
+
+        public int Name { get; set; }
+    }
+    internal class Program
+    {
+        static Program()
+        {
+            A.NUM = 356;
+            Console.WriteLine(A.NUM);
+        }
+        static void Main(string[] args)
+        {
+            
+            Console.Read();
+        }
+    }
+}
