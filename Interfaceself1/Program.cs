d1f150f49fea8b0a02364c25b82cf4f1b563a0a4
Nishant Bhatt <nishantbhatt393@gmail.com>
2025-04-15 14:01:41 +0530

    changes in interfaceself1

--- Interfaceself1/Program.cs
+++ Interfaceself1/Program.cs
@@ -13,10 +13,49 @@ namespace Interfaceself1
         void Describe();
         void Describe(string Name);
     }
+    public class B:methods
+    {
+        void methods.Describe() { Console.WriteLine("explicitely implemented"); }
+        void methods.Describe(string Name) {
+            Console.WriteLine("Explicitely implemented Name");
+        }
+        string methods.Name { get; }
+        string methods.Description { get; }
+
+    }
+    public class A:methods
+    {
+        public string Name { get; }
+        public string Description { get; }
+        public void Describe()
+        {
+            Console.WriteLine("Im the default method from the interface" +
+                "implemented in class A");
+        }
+        public void Describe(string Name)
+        {
+            Console.WriteLine("Im the argumented method from the interface " +
+                "implemented in class A");
+
+        }
+
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            A a = new A();
+            a.Describe();
+            a.Describe("Nishant");
+            methods b = new A();
+            b.Describe(); 
+            b.Describe("Nishant");
+            B b2 = new B();
+            b = b2;
+            b.Describe();
+            b.Describe("Hello");
+            Console.Read();
+            
         }
     }
 }
