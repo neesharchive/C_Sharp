42f674843b7fdbb0753599fedc657269649fe1ae
Nishant Bhatt <nishantbhatt393@gmail.com>
2025-04-22 15:05:38 +0530

    Delegates and partial class 1

---
+++ DelegateSingle/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateSingle
+{
+    partial class B
+    {
+        static int y=23;
+    }
+}
+
+namespace DelegateSingle
+{
+
+    partial class B
+    {
+        static int x=12;
+
+        public static void show()
+        {
+            Console.WriteLine($"Here is it: {x} and {y}");
+        }
+    }
+
+    public delegate void Del1();
+
+    interface m
+    {
+        void m1();
+        void m2();
+    }
+    public class A : m
+    {
+        public void m1()
+        {
+            Console.WriteLine("Method m1");
+        }
+        public void m2()
+        {
+            Console.WriteLine("Method m2");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            B.show();
+
+            Del1 d1;
+            A a1= new A();
+            d1 = a1.m1;
+            d1();
+            d1 = a1.m2;
+            d1();
+
+            Console.Read();
+        }
+    }
+}
---
+++ DelegatesMulti/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesMulti
+{
+    
+    internal class Program
+    {   
+        public delegate void del1();
+        public static void m1()
+        {
+            Console.WriteLine("The method1");
+        }
+        public static void m2()
+        {
+            Console.WriteLine("The method2");
+        }
+        static void Main(string[] args)
+        {
+            del1 d = new del1(m1);
+            d();
+            d += m2;
+            d+= m1;
+            d += m2;
+            d += m1;
+            d += m2;
+            d -= m1;
+            d -= m2;
+            d -= m1;
+            d -= m2;
+            d();
+
+            Console.Read();
+        }
+    }
+}
